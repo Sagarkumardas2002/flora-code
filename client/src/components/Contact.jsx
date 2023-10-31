70649c88adc023420d4966ded0462ad2a6628fda
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2023-10-31 14:10:25 +0530

    Update Contact.jsx

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:5050/api/auth/register", {
+      const response = await fetch("https://flora-backend.onrender.com/api/auth/register", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
